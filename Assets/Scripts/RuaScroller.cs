82adc0ef8b1bb252ba4d9b252f413af8dfc6a1c8
Eric-Catarina <2020954200@teiacoltec.org>
2022-07-08 19:13:51 -0300

    Adição de Música de Fundo + Pequena melhora na legibilidade do código

--- Assets/Scripts/RuaScroller.cs
+++ Assets/Scripts/RuaScroller.cs
@@ -1,34 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class RuaScroller : MonoBehaviour
 {
     [SerializeField]
-   private GameObject ruaGO;
-    [SerializeField]
-   private GameObject ruaPrincipal;
-   [SerializeField]
-   private Transform Pai;
+    private GameObject ruaGameObject;
 
-   private RectTransform rt;
-   private Vector2 posicaoInicial = new Vector2(Screen.width/2, Screen.height*1.5f );
-   private Vector2 posicaoInicialDaRuaPrincipal;
-   private GameObject instanciaDaRuaGO;
+    [SerializeField] private GameObject ruaPrincipal;
+    [SerializeField] private Transform Pai;
+    private Vector2 posicaoInicialDaCopiaDaRua;
+
+    private void setaPosicaoInicialDaCopiaDaRua()
+    {
+        posicaoInicialDaCopiaDaRua = new Vector2(Screen.width / 2, Screen.height * 1.5f);
+    }
+    
+    private Vector2 posicaoInicialDaRuaPrincipal;
+    private GameObject instanciaDaRuaGO;
+
     void Start()
     {
-        rt = GetComponent<RectTransform>();
-        instanciaDaRuaGO = Instantiate(ruaGO,Pai);
-        instanciaDaRuaGO.transform.position =new Vector2(Screen.width/2, Screen.height*1.5f ); 
+        setaPosicaoInicialDaCopiaDaRua();
+        instanciaDaRuaGO = Instantiate(ruaGameObject, Pai);
+        instanciaDaRuaGO.transform.position = new Vector2(Screen.width / 2, Screen.height * 1.5f);
         posicaoInicialDaRuaPrincipal = ruaPrincipal.transform.position;
-
     }
+
     void Update()
     {
-        if(instanciaDaRuaGO.transform.position.y<Screen.height/2f){
-            instanciaDaRuaGO.transform.position = posicaoInicial;
+        if (instanciaDaRuaGO.transform.position.y < Screen.height / 2f)
+        {
+            instanciaDaRuaGO.transform.position = posicaoInicialDaCopiaDaRua;
             ruaPrincipal.transform.position = posicaoInicialDaRuaPrincipal;
         }
-    
     }
 }
