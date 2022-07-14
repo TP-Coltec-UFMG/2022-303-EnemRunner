7309bc87b0ead55b23c679d3e174f9d8234c27fa
Eric-Catarina <2020954200@teiacoltec.org>
2022-07-14 13:23:16 -0300

    Adição do botão play Again ao morrer

--- Assets/Scripts/ControlePlayer.cs
+++ Assets/Scripts/ControlePlayer.cs
@@ -3,15 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class ControlePlayer : MonoBehaviour
 {
-
     private ManagerDeSons soundManager;
     private float playerY = Screen.height * 2 / 10;
     private float posicaoColuna = 2;
 
-    
+    private JogarDeNovo_OuMenu JogarDeNovoMenu;
+
     private PosicaoDasColunas Colunas;
 
     public enum colunaAtual
@@ -19,34 +20,31 @@ public class ControlePlayer : MonoBehaviour
         colunaUm,
         colunaDois,
         colunaTres
-
     };
 
     private colunaAtual playerColunaAtual;
-    
+
     private float colunaUmX;
     private float colunaDoisX;
     private float colunaTresX;
-
 
 
     private PosicaoDasColunas TresColunas;
     private RectTransform rt;
 
     
-
     private void MoveEsquerda()
     {
         posicaoColuna--;
         posicaoColuna = Mathf.Max(posicaoColuna, 1);
-
-
     }
+
     private void MoveDireita()
     {
         posicaoColuna++;
         posicaoColuna = Mathf.Min(posicaoColuna, 3);
     }
+
     private void SalvaPosicoesDasColunas()
     {
         TresColunas = Colunas.GetComponent<PosicaoDasColunas>();
@@ -55,6 +53,7 @@ public class ControlePlayer : MonoBehaviour
         colunaDoisX = TresColunas.colunaDoisX;
         colunaTresX = TresColunas.colunaTresX;
     }
+
     private void MovimentoHorizontal()
     {
         if (posicaoColuna == 1)
@@ -62,17 +61,18 @@ public class ControlePlayer : MonoBehaviour
             playerColunaAtual = colunaAtual.colunaUm;
             rt.transform.position = new Vector2(colunaUmX, rt.transform.position.y);
         }
+
         if (posicaoColuna == 2)
         {
             playerColunaAtual = colunaAtual.colunaDois;
             rt.transform.position = new Vector2(colunaDoisX, rt.transform.position.y);
         }
+
         if (posicaoColuna == 3)
         {
             playerColunaAtual = colunaAtual.colunaTres;
             rt.transform.position = new Vector2(colunaTresX, rt.transform.position.y);
         }
-
     }
 
     public colunaAtual RetornaColunaAtualPlayer()
@@ -80,16 +80,19 @@ public class ControlePlayer : MonoBehaviour
         return playerColunaAtual;
     }
 
-    private void OnCollisionEnter2D (Collision2D obstaculoColidido)
+    private void OnCollisionEnter2D(Collision2D obstaculoColidido)
     {
         Time.timeScale = 0;
         soundManager.TocaSomImpacto();
         soundManager.ParaMusicaBackground();
-        
+        JogarDeNovoMenu.gameObject.SetActive(true);
+        JogarDeNovoMenu.CarregaTelaGameOver();
     }
-
+    
     void Start()
     {
+        JogarDeNovoMenu = GameObject.FindObjectOfType<JogarDeNovo_OuMenu>();
+        JogarDeNovoMenu.gameObject.SetActive(false);
         Colunas = GameObject.FindObjectOfType<PosicaoDasColunas>();
         soundManager = GameObject.FindObjectOfType<ManagerDeSons>();
         playerColunaAtual = colunaAtual.colunaDois;
@@ -97,7 +100,7 @@ public class ControlePlayer : MonoBehaviour
         rt.transform.position = new Vector2(rt.transform.position.x, playerY);
         SalvaPosicoesDasColunas();
     }
-    
+
     public void OnMovimento(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
@@ -108,14 +111,16 @@ public class ControlePlayer : MonoBehaviour
             {
                 MoveEsquerda();
             }
+
             if (direcao > 0)
             {
                 MoveDireita();
             }
+
             MovimentoHorizontal();
-
-
         }
+        
+
 
     }
 }
---
+++ Assets/Scripts/JogarDeNovo_OuMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JogarDeNovo_OuMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameOverGameObject;
+    private ControlePlayer jogador;
+
+    public void CarregaMenuInicial()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void CarregaTelaGameOver()
+    {
+        gameOverGameObject.SetActive(true);
+    }
+    
+    void Start()
+    {
+        jogador = GameObject.FindObjectOfType<ControlePlayer>();
+    }
+
+
+    
+    void Update()
+    {
+        
+    }
+}
