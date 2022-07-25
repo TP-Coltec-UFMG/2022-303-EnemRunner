5bdc20063ad879159f3920c54bcd920135fdab71
Eric-Catarina <2020954200@teiacoltec.org>
2022-07-25 12:43:30 -0300

    Barra Lateral Funcionando

--- Assets/Scripts/BarraLateral.cs
+++ Assets/Scripts/BarraLateral.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 public class BarraLateral : MonoBehaviour
 {
     private Slider barraLateral;
-
+    [SerializeField]
+    private float FillSpeed = 0.1f;
+    private float progressoAlvo = 0;
     private void Awake() {
         barraLateral = gameObject.GetComponent<Slider>();
 
@@ -13,12 +15,21 @@ public class BarraLateral : MonoBehaviour
 
     void Start()
     {
-        
+        IncrementProgress(1f);
     }
 
     
     void Update()
     {
-        
+        if (barraLateral.value < progressoAlvo)
+        {
+            barraLateral.value += FillSpeed * Time.deltaTime;
+        }   
     }
+
+    public void IncrementProgress(float newProgress)
+    {
+        progressoAlvo = barraLateral.value + newProgress;
+    }
+
 }
